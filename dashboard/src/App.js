d903eb0a8d7e231b411681f26f33cd4a6e30b0ee
Reigniel Cabela <Reignielcrg@gmail.com>
2024-10-28 15:29:21 +0800

    About

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import Main from './components/Main';
 import Sidebar from './components/Sidebar';
 
+import About from './components/About';
+
+
 function App() {
   return (
     <div className='h-screen bg-gradient-to-r from-[#ce6f6f] to-[#29d38c] p-2 font-poppins'>
       <div className='h-full bg-white bg-opacity-50 shadow-md rounded-3xl flex gap-6 p-6'>
         <Sidebar />
-        <Main />
+        <div className='flex flex-col gap-6 h-full w-full text-gray-900 rounded-xl overflow-y-scroll'> 
+          <Main />
+          <About />
+        </div>
       </div>
     </div>
     
