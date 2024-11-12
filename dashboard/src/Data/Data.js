d9594a2fbfc24d89409850eb2749dcde307a1a53
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2024-11-13 03:09:34 +0800

    Counted Users and Computed the Percentage.

--- dashboard/src/Data/Data.js
+++ dashboard/src/Data/Data.js
@@ -53,9 +53,9 @@ export const cardsDataMicro = [
       },
       barValue: 70,
       url:"https://public.tableau.com/views/PlayingwithTableausNewSpatialParameter/Map?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
-      value: "25,970",
-      png: UilChart,
-      percent: "25%",
+      value: "10,107", /*1778 + 1800 + 1824 + 1558 + 1573 + 1574*/ /*Monthly report*/
+      png: UilChart,   /*PercentIncrease = [(newvalue - originalvalue)/originalvalue] x 100*/
+      percent: "0%",  //                      [(1574-1573) / 1573] x 100 = 0.063
     },
     {
       title: "Personal Loan",
@@ -64,9 +64,9 @@ export const cardsDataMicro = [
       },
       barValue: 70,
       url:"https://public.tableau.com/views/PlayingwithTableausNewSpatialParameter/Map?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
-      value: "25,970",
+      value: "2,660", /*444 + 446 + 443 + 442 + 440 + 445*/
       png: UilChart,
-      percent: "70%",
+      percent: "1%", // [(445-440) / 440] x 100 = 1.13%
 
     },
     {
@@ -76,9 +76,9 @@ export const cardsDataMicro = [
       },
       barValue: 70,
       url:"https://public.tableau.com/views/PlayingwithTableausNewSpatialParameter/Map?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
-      value: "25,970",
+      value: "275", /*31 + 32 + 33 + 58 + 59 + 62*/
       png: UilChart,
-      percent: "70%",
+      percent: "5%", // [(62-59) / 59] x 100 = 5.08%
 
     },
     {
@@ -89,9 +89,9 @@ export const cardsDataMicro = [
       },
       barValue: 70,
       url:"https://public.tableau.com/views/PlayingwithTableausNewSpatialParameter/Map?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
-      value: "25,970",
+      value: "132", /*22 + 22 + 22 + 22 + 22 + 22*/
       png: UilChart,
-      percent: "70%",
+      percent: "0%", // [(22-22) / 22] x 100 = 0% 
 
     },
 ];
@@ -104,9 +104,9 @@ export const cardsDataSmall = [
     },
     barValue: 70,
     url:"https://public.tableau.com/views/PlayingwithTableausNewSpatialParameter/Map?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
-    value: "25,970",
+    value: "9,864", /*1677 + 1672 + 1671 + 1617 + 1618 + 1609*/
     png: UilChart,
-    percent: "25%",
+    percent: "0%", // [(1609-1618) / 1618] x 100 = -0.55%
   },
   {
     title: "Personal Loan",
@@ -115,9 +115,9 @@ export const cardsDataSmall = [
     },
     barValue: 70,
     url:"https://public.tableau.com/views/PlayingwithTableausNewSpatialParameter/Map?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
-    value: "25,970",
+    value: "5,396", /*909 + 912 + 900 + 889 + 891 + 895*/
     png: UilChart,
-    percent: "70%",
+    percent: "0%", // [(895-891) / 891] x 100 = 0.44%
 
   },
   {
@@ -127,9 +127,9 @@ export const cardsDataSmall = [
     },
     barValue: 70,
     url:"https://public.tableau.com/views/PlayingwithTableausNewSpatialParameter/Map?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
-    value: "25,970",
+    value: "659", /*67 + 72 + 75 + 146 + 149 + 150*/
     png: UilChart,
-    percent: "70%",
+    percent: "1%", // [(150-149) / 149] x 100 = 0.67%
 
   },
   {
@@ -140,9 +140,9 @@ export const cardsDataSmall = [
     },
     barValue: 70,
     url:"https://public.tableau.com/views/PlayingwithTableausNewSpatialParameter/Map?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
-    value: "25,970",
+    value: "555", /*92 + 93 + 91 + 93 + 94 + 92*/
     png: UilChart,
-    percent: "70%",
+    percent: "0%", // [(92-94) / 94] x 100 = -2.12% 
 
   },
 
@@ -155,9 +155,9 @@ export const cardsDataMedium = [
     },
     barValue: 70,
     url:"https://public.tableau.com/views/PlayingwithTableausNewSpatialParameter/Map?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
-    value: "25,970",
+    value: "8,337", /*1403 + 1402 + 1402 + 1380 + 1376 + 1374*/
     png: UilChart,
-    percent: "25%",
+    percent: "0%", // [(1374-1376) / 1376] x 100 = -0.14%
   },
   {
     title: "Personal Loan",
@@ -166,9 +166,9 @@ export const cardsDataMedium = [
     },
     barValue: 70,
     url:"https://public.tableau.com/views/PlayingwithTableausNewSpatialParameter/Map?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
-    value: "25,970",
+    value: "6,694", /*1116 + 1118 + 1108 + 1113 + 1117 + 1122*/
     png: UilChart,
-    percent: "70%",
+    percent: "0%", // [(1122-1117) / 1117] x 100 = 0.44%
 
   },
   {
@@ -178,9 +178,9 @@ export const cardsDataMedium = [
     },
     barValue: 70,
     url:"https://public.tableau.com/views/PlayingwithTableausNewSpatialParameter/Map?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
-    value: "25,970",
+    value: "1,423", /*194 + 198 + 206 + 268 + 274 + 283*/
     png: UilChart,
-    percent: "70%",
+    percent: "3%", // [(283-274) / 274] x 100 = 3.28%
 
   },
   {
@@ -191,9 +191,9 @@ export const cardsDataMedium = [
     },
     barValue: 70,
     url:"https://public.tableau.com/views/PlayingwithTableausNewSpatialParameter/Map?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
-    value: "25,970",
+    value: "1,502", /*251 + 250 + 247 + 249 + 250 + 255*/
     png: UilChart,
-    percent: "70%",
+    percent: "2%", // [(255-250) / 250] x 100 = 2%
 
   },
 ];
